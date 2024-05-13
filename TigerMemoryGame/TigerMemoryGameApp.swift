105efb84a143c44a21671be1d2ebe951ad2572a4
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-05-13 20:58:26 +0300

    Initial Commit

---
+++ TigerMemoryGame/TigerMemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TigerMemoryGameApp.swift
+//  TigerMemoryGame
+//
+//  Created by Роман on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TigerMemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
